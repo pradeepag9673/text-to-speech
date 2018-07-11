335c09ea9832f7a94473fb532b248381aa1cdcb8
Pradeep A G <pradeep_ga@photonint.com>
2018-07-11 19:50:54 +0530

    Initial commit

---
+++ src/main/java/com/polly/texttospeech/TextToSpeechApplication.java
@@ -0,0 +1,20 @@
+package com.polly.texttospeech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author pradeep_ga
+ * This application consumes plaintext and converts text to speech using AmazonPolly.
+ *
+ */
+@SpringBootApplication
+public class TextToSpeechApplication {
+
+	/**
+	 * @param args - command line args...
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(TextToSpeechApplication.class, args);
+	}
+}
---
+++ src/test/java/com/polly/texttospeech/TextToSpeechApplicationTests.java
@@ -0,0 +1,23 @@
+package com.polly.texttospeech;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author pradeep_ga
+ *
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TextToSpeechApplicationTests {
+
+	/**
+	 * 
+	 */
+	@Test
+	public void contextLoads() {
+	}
+
+}
